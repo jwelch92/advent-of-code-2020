6a5fef1748e71316bdfb7cd1b22b7cdd6e610c3c
Jared Welch <jwelch@newrelic.com>
2020-12-04 16:27:11 -0800

    Little bit of code golfing to reduce the LOC

--- days/004/day_004.py
+++ days/004/day_004.py
@@ -1,31 +1,17 @@
 import re
-from typing import List
-
-
-def validate_hgt(hgt: str) -> bool:
-    # hgt (Height) - a number followed by either cm or in:
-    # If cm, the number must be at least 150 and at most 193.
-    # If in, the number must be at least 59 and at most 76.
-    if "cm" in hgt:
-        return 150 <= int(hgt.replace("cm", "")) <= 193
-    elif "in" in hgt:
-        return 59 <= int(hgt.replace("in", "")) <= 76
-    else:
-        return False
-
-
-hcl_re = re.compile(r'#[a-z0-9]{6}')
-ecl_values = "amb blu brn gry grn hzl oth".split(" ")
+from typing import List, Dict
 
 fields = [
-    ("byr:", True, lambda x: 1920 <= int(x) <= 2002),  # four digits; at least 1920 and at most 2002.
-    ("iyr:", True, lambda x: 2010 <= int(x) <= 2020),  # four digits; at least 2010 and at most 2020.
-    ("eyr:", True, lambda x: 2020 <= int(x) <= 2030),  # four digits; at least 2020 and at most 2030.
-    ("hgt:", True, validate_hgt),
-    ("hcl:", True, lambda x: hcl_re.match(x)),  # a # followed by exactly six characters 0-9 or a-f.
-    ("ecl:", True, lambda x: x in ecl_values),  # exactly one of: amb blu brn gry grn hzl oth.
-    ("pid:", True, lambda x: len(x) == 9 and int(x.lstrip("0"))),  # a nine-digit number, including leading zeroes.
-    ("cid:", False, lambda x: x),  # ignored, missing or not.
+    ("byr", True, lambda x: 1920 <= int(x) <= 2002),  # four digits; at least 1920 and at most 2002.
+    ("iyr", True, lambda x: 2010 <= int(x) <= 2020),  # four digits; at least 2010 and at most 2020.
+    ("eyr", True, lambda x: 2020 <= int(x) <= 2030),  # four digits; at least 2020 and at most 2030.
+    ("hgt", True, lambda x: 150 <= int(x.replace("cm", "")) <= 193 if "cm" in x else 59 <= int(
+        x.replace("in", "")) <= 76 if "in" in x else False),
+    ("hcl", True, lambda x: re.match(r'#[a-z0-9]{6}', x)),  # a # followed by exactly six characters 0-9 or a-f.
+    ("ecl", True, lambda x: x in "amb blu brn gry grn hzl oth".split(" ")),
+    # exactly one of: amb blu brn gry grn hzl oth.
+    ("pid", True, lambda x: len(x) == 9 and int(x.lstrip("0"))),  # a nine-digit number, including leading zeroes.
+    ("cid", False, lambda x: x),  # ignored, missing or not.
 ]
 
 
@@ -55,28 +41,25 @@ def validate_part_one(passport) -> bool:
 
 def part_one():
     print('Part one')
-    lines = read()
-    valid = [x for x in lines if validate_part_one(x)]
+    valid = [x for x in read() if validate_part_one(x)]
     print("Total valid", len(valid))
 
 
-def get_field(passport: str, field: str):
-    for c in passport.split(" "):
-        if c.startswith(field):
-            return c.split(":")[-1]
+def to_fields(passport: str) -> Dict[str, str]:
+    return {x.split(":")[0]: x.split(":")[1] for x in passport.split(" ")}
 
 
 def validate_part_two(passport: str) -> bool:
+    p = to_fields(passport)
     for field, required, validator in fields:
-        if required and (field not in passport or not validator(get_field(passport, field))):
+        if required and (field not in p.keys() or not validator(p[field])):
             return False
     return True
 
 
 def part_two():
     print('Part two')
-    lines = read()
-    valid = [x for x in lines if validate_part_two(x)]
+    valid = [x for x in read() if validate_part_two(x)]
     print("Total valid", len(valid))
 
 
