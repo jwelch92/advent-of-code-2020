7f6f4784a45fe26db0377fcf2a37a33faf8729c1
Jared Welch <jwelch@newrelic.com>
2020-12-08 21:19:26 -0800

    day 9 part 1. part 2 will have to wait until after sleep

---
+++ days/009/day_009.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from typing import List
+from itertools import combinations
+
+import parse
+
+
+def read() -> List[int]:
+    with open('input.txt') as f:
+        return [int(x.strip()) for x in f.readlines()]
+
+
+def part_one():
+    print('Part one')
+    xmas = read()
+    for i in range(25, len(xmas)):
+        print(i)
+        cur = xmas[i]
+        c = combinations(xmas[i-25:i], 2)
+
+        for a, b in c:
+            if a + b == cur:
+                break
+        else:
+            print("did not find a pair for", cur)
+            break
+
+
+
+
+def part_two():
+    print('Part two')
+    # --- Part Two ---
+    # The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
+    #
+    # Again consider the above example:
+    #
+    # 35
+    # 20
+    # 15
+    # 25
+    # 47
+    # 40
+    # 62
+    # 55
+    # 65
+    # 95
+    # 102
+    # 117
+    # 150
+    # 182
+    # 127
+    # 219
+    # 299
+    # 277
+    # 309
+    # 576
+    # In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+    #
+    # To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
+    #
+    # What is the encryption weakness in your XMAS-encrypted list of numbers?
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
