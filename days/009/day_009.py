09ffb0f8082e336009c2356cbfe0da1d0beab544
Jared Welch <jwelch@newrelic.com>
2020-12-11 13:30:32 -0800

    clean up day 9

--- days/009/day_009.py
+++ days/009/day_009.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
-from typing import List
 from itertools import combinations
-
-import parse
+from typing import List
 
 
 def read() -> List[int]:
@@ -16,7 +13,7 @@ def part_one():
     for i in range(25, len(xmas)):
         print(i)
         cur = xmas[i]
-        c = combinations(xmas[i-25:i], 2)
+        c = combinations(xmas[i - 25:i], 2)
 
         for a, b in c:
             if a + b == cur:
@@ -26,41 +23,9 @@ def part_one():
             break
 
 
-
-
 def part_two():
     print('Part two')
-    # --- Part Two ---
-    # The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous set of at least two numbers in your list which sum to the invalid number from step 1.
-    #
-    # Again consider the above example:
-    #
-    # 35
-    # 20
-    # 15
-    # 25
-    # 47
-    # 40
-    # 62
-    # 55
-    # 65
-    # 95
-    # 102
-    # 117
-    # 150
-    # 182
-    # 127
-    # 219
-    # 299
-    # 277
-    # 309
-    # 576
-    # In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
-    #
-    # To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example, these are 15 and 47, producing 62.
-    #
-    # What is the encryption weakness in your XMAS-encrypted list of numbers?
-
+    # my answer from part one
     magic = 373803594
 
     xmas = read()
@@ -72,20 +37,11 @@ def part_two():
             running += xmas[running_index]
             running_index += 1
         if running == magic:
-            print("found")
-            print(i, running_index)
             r = xmas[i: running_index]
-            print(r)
-            print(sum(r))
-            print(r == magic)
-
-
             print("ans", min(r) + max(r))
             break
 
 
-
-
 if __name__ == '__main__':
-    # part_one()
+    part_one()
     part_two()
