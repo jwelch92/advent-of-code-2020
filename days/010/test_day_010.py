b0cf15df9b3b6abbc5691e504e30fb64f0526183
Jared Welch <jwelch@newrelic.com>
2020-12-11 14:37:53 -0800

    WIP, tests for day 10, start on day 11

--- days/010/test_day_010.py
+++ days/010/test_day_010.py
@@ -2,10 +2,7 @@ import unittest
 
 from .day_010 import solve_one, solve_two, parse
 
-
-class MyTestCase(unittest.TestCase):
-    def test_part_one(self):
-        puzzle_input = """28
+puzzle_input = """28
 33
 18
 42
@@ -38,13 +35,14 @@ class MyTestCase(unittest.TestCase):
 3
 """
 
+
+class MyTestCase(unittest.TestCase):
+    def test_part_one(self):
         expected_output = 220
         self.assertEqual(solve_one(parse(puzzle_input.splitlines())), expected_output)
 
-
     def test_part_two(self):
-        pass
-
+        self.assertEqual(solve_two(parse(puzzle_input.splitlines()), 0), 19208)
 
 
 if __name__ == '__main__':
--- days/011/day_011.py
+++ days/011/day_011.py
@@ -1,45 +1,56 @@
-from itertools import combinations
-from typing import List
+import itertools
+
+with open('input.txt') as f:
+    data = f.read().splitlines()
+
+floor, ec, oc, = ".", "L", "#"
 
 
-def read() -> List[int]:
-    with open('input.txt') as f:
-        return [int(x.strip()) for x in f.readlines()]
+def neighbors(a, radius, rowNumber, columnNumber):
+    return [[a[i][j] if i >= 0 and i < len(a) and j >= 0 and j < len(a[0]) else 0
+             for j in range(columnNumber - 1 - radius, columnNumber + radius)]
+            for i in range(rowNumber - 1 - radius, rowNumber + radius)]
+
+
+print(data)
+
+test = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14]
+]
+
+
+def nearby(a, x, y):
+    rows = len(a)
+    cols = len(a[0])
+    # handle edges
+    left = a[x][y - 1]
+    right = a[x][y + 1]
+    top = a[x - 1][y]
+    down = a[x + 1][y]
+
+    nw = [x - 1][y - 1]
+
+
+i, j = 2, 2
+n = list(itertools.product(range(i - 1, i + 2), range(j - 1, j + 2)))
+
+for a, b in n:
+    print(a, b, test[a][b])
+
+
+# print(neighbors(test, 1, 1, 1))
+# interesting approach here https://stackoverflow.com/questions/26363579/how-to-find-neighbors-of-a-2d-list-in-python/26363975
 
 
 def part_one():
     print('Part one')
-    xmas = read()
-    for i in range(25, len(xmas)):
-        print(i)
-        cur = xmas[i]
-        c = combinations(xmas[i - 25:i], 2)
-
-        for a, b in c:
-            if a + b == cur:
-                break
-        else:
-            print("did not find a pair for", cur)
-            break
 
 
 def part_two():
     print('Part two')
     # my answer from part one
-    magic = 373803594
-
-    xmas = read()
-    count = len(xmas)
-    for i in range(0, count):
-        running = 0
-        running_index = i
-        while running < magic:
-            running += xmas[running_index]
-            running_index += 1
-        if running == magic:
-            r = xmas[i: running_index]
-            print("ans", min(r) + max(r))
-            break
 
 
 if __name__ == '__main__':
