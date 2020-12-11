9d2e193f40d1bc2b9582f7de17d488670cdaf411
Jared Welch <jwelch@newrelic.com>
2020-12-11 15:24:47 -0800

    wip getting close. Need to not mutate within the loop
    > The following rules are applied to every seat simultaneously:

--- days/011/day_011.py
+++ days/011/day_011.py
@@ -1,58 +1,82 @@
 import itertools
+import copy
+from pprint import pprint as pp
 
 with open('input.txt') as f:
     data = f.read().splitlines()
 
 floor, ec, oc, = ".", "L", "#"
 
+test = """L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"""
 
-def neighbors(a, radius, rowNumber, columnNumber):
-    return [[a[i][j] if i >= 0 and i < len(a) and j >= 0 and j < len(a[0]) else 0
-             for j in range(columnNumber - 1 - radius, columnNumber + radius)]
-            for i in range(rowNumber - 1 - radius, rowNumber + radius)]
+test = [list(x) for x in test.splitlines()]
 
+around = list(itertools.product(range(-1, 2), range(-1, 2)))
+around.remove((0, 0))
+print(around)
 
-print(data)
-
-test = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14]
-]
-
-
-def nearby(a, x, y):
-    rows = len(a)
-    cols = len(a[0])
-    # handle edges
-    left = a[x][y - 1]
-    right = a[x][y + 1]
-    top = a[x - 1][y]
-    down = a[x + 1][y]
-
-    nw = [x - 1][y - 1]
+# print(a, b, test[a][b])
 
 
-i, j = 2, 2
-n = list(itertools.product(range(i - 1, i + 2), range(j - 1, j + 2)))
 
-for a, b in n:
-    print(a, b, test[a][b])
+iterations = 0
+previous = []
+working = test
+while True:
+    print("Starting round", iterations)
+    pp(working)
+    rows = len(working)
+    cols = len(working[0])
+
+    for i in range(0, rows):
+        for j in range(0, cols):
+            print("checking", i, j)
+
+            if working[i][j] == floor:
+                continue
+
+            occupied = 0
+            for a, b in around:
+                row = i + a
+                col = j + b
+                if (row >= 0 and col >= 0) and (row < rows and col < cols):
+                    print(row, col, working[row][col])
+                    occupied += int(working[row][col] == oc)
+            print(occupied)
+            if working[i][j] == ec and occupied == 0:
+                working[i][j] = oc
+                continue
+            if working[i][j] == oc and occupied >= 4:
+                working[i][j] = ec
+                continue
+
+    iterations += 1
+    if working == previous:
+        pp(working)
+        pp(previous)
+        break
+    print("not the same, copying")
+    previous = copy.deepcopy(working)
 
 
-# print(neighbors(test, 1, 1, 1))
-# interesting approach here https://stackoverflow.com/questions/26363579/how-to-find-neighbors-of-a-2d-list-in-python/26363975
-
-
-def part_one():
-    print('Part one')
-
-
-def part_two():
-    print('Part two')
-    # my answer from part one
-
-
-if __name__ == '__main__':
-    part_one()
-    part_two()
+# def part_one():
+#     print('Part one')
+#
+#
+# def part_two():
+#     print('Part two')
+#     # my answer from part one
+#
+#
+# if __name__ == '__main__':
+#     part_one()
+#     part_two()
