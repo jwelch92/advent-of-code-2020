3f2e41edad77cbd57a842c4cd6a997b0393bf378
Jared Welch <jwelch@newrelic.com>
2020-12-13 22:14:35 -0800

    day 14 is weird

---
+++ days/014/day_014.py
@@ -0,0 +1,94 @@
+from collections import defaultdict
+from typing import List
+from functools import reduce
+import parse
+from bitarray import bitarray, util
+mask_pat = parse.compile("mask = {}")
+set_pat = parse.compile("mem[{}] = {}")
+
+
+def parser(input):
+    print(input)
+    instructions = []
+    acc = [mask_pat.parse(input[0]).fixed[0]]
+    for line in input[1:]:
+        if "mask" in line:
+            instructions.append(acc)
+            acc = [mask_pat.parse(line).fixed[0]]
+        else:
+            addr, val = map(int, set_pat.parse(line).fixed)
+            acc.append((addr, val))
+    instructions.append(acc)
+    return instructions
+
+
+big_int = 2 ** 36
+
+def bitmask(mask):
+    return []
+
+def read() -> List[str]:
+    with open("input.txt") as f:
+        return f.read().splitlines()
+
+def apply_mask(mask, val):
+    pass
+
+# def solve_one(puzzle):
+#     mem = defaultdict(int)
+#     for chunk in puzzle:
+#         mask, instr = chunk[0], chunk[1:]
+#         for addr, value in instr:
+#             print(addr, value)
+#             val = 0
+#             for i in range(36):
+#                 c = mask[-i-1]
+#                 print(c)
+#                 # if 1 in mask or a high bit X then OR the bit
+#                 if c == "1" or (c == 'X' and (value & (1 << i)) > 0):
+#                     val |= (1 << i)
+#             mem[addr] = val
+#             print(val)
+#     return sum(mem.values())
+
+# dumb attempt at using bitarry to do it "right" but loops and lists are easier actually
+# def solve_one(puzzle):
+#     mem = {}
+#     for s in puzzle:
+#         mask, instr = s[0], s[1:]
+#         m = list(mask)
+#         for i in instr:
+#             print(i)
+#             ba = util.int2ba(i[1], 36, endian="big")
+#             print(ba)
+#             for masked, (index, value) in zip(m, enumerate(ba)):
+#                 if masked == "X":
+#                     continue
+#                 if int(masked) == value:
+#                     continue
+#                 ba[index] |= int(masked)
+#             print("masked", ba)
+#             to_int = util.ba2int(ba)
+#             print(to_int)
+#             mem[i[0]] = to_int
+#     print(mem)
+#     return sum(mem.values())
+
+
+
+def part_one():
+    print('Part one')
+    p = parser(read())
+    print(p)
+    ans = solve_one(p)
+    print("ans", ans)
+
+
+def part_two():
+    print('Part two')
+
+
+
+if __name__ == '__main__':
+    part_one()
+    part_two()
---
+++ days/014/test_day_014.py
@@ -0,0 +1,27 @@
+import unittest
+
+from .day_014 import parser, solve_one
+
+puzzle_input = """mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0
+""".splitlines()
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_parse(self):
+        e = [["XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", (8, 11), (7, 101), (8, 0)]]
+        self.assertEqual(e, parser(puzzle_input))
+
+    def test_part_one(self):
+        p = parser(puzzle_input)
+        self.assertEqual(solve_one(p), 165)
+
+    def test_part_two(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
